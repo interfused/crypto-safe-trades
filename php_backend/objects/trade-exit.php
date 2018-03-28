c94043a0fe15a7f469adb19f29db3cb75acd7731
interfused <jeremy@interfused-inc.com>
2018-03-28 08:29:08 -0400

    fix comments

--- php_backend/objects/trade-exit.php
+++ php_backend/objects/trade-exit.php
@@ -1,6 +1,4 @@
 <?php
-//https://www.codeofaninja.com/2017/02/create-simple-rest-api-in-php.html
-//https://www.phpflow.com/php/create-php-restful-api-without-rest-framework-dependency/
 
 class TradeExit{
   //database connection and table name
